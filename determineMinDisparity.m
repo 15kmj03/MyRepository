90fcf0d66c858549651776c8a542d6ba7c6f88f7
15kmj03 <15kmj03@ms.dendai.ac.jp>
2016-12-25 17:48:22 +0900

    Update determineMinDisparity.m

--- determineMinDisparity.m
+++ determineMinDisparity.m
@@ -1,20 +1,12 @@
-function [ minDisparity,prevBbox ] = determineMinDisparity( grayL,grayR )
-%DETERMINEDISPARITYRANGE この関数の概要をここに記述
-%   詳細説明をここに記述
+function [ minDisparity ] = determineMinDisparity( grayL,grayR,bbox )
+%DETERMINEDISPARITYRANGE 縺薙�ｮ髢｢謨ｰ縺ｮ讎りｦ√ｒ縺薙％縺ｫ險倩ｿｰ
+%   隧ｳ邏ｰ隱ｬ譏弱ｒ縺薙％縺ｫ險倩ｿｰ
 %
-% 視差計算範囲 64
+% 隕門ｷｮ險育ｮ礼ｯ�蝗ｲ 64
 % disp-32 < disp < disp+32
 
-faceDetector = vision.CascadeObjectDetector('MinSize',[200,200],...
-    'MaxSize',[400,400]);
-
-bboxs=step(faceDetector,grayL);
-if ~isempty(bboxs)
-    bbox=bboxs(1,:);
-    disp=calculateDisparity(grayL,grayR,bbox);
-    minDisparity=disp-(8*4);
-    prevBbox=bbox;
-end
+disp=calculateDisparity(grayL,grayR,bbox);
+minDisparity=disp-(8*4);
     
 end
 
